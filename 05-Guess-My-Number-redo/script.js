42ade7c014a3a500a817aec44140a6e9146862b8
rfd10 <rfd10@duke.edu>
2023-04-14 18:45:54 -0400

    guess my number redo done

--- 05-Guess-My-Number-redo/script.js
+++ 05-Guess-My-Number-redo/script.js
@@ -1,33 +1,37 @@
 let score = 20;
 let highestScore = 0;
 let number;
+let lost = 0;
+const messageElement = document.querySelector(".message");
+const bodyElement = document.querySelector("body");
 
 generateRandom(number);
 
 function generateRandom() {
   number = Math.trunc(Math.random() * 20 + 1);
-  document.querySelector(".number").textContent = number;
 }
 
 function verifyGuess() {
-  let guess = document.querySelector(".myguess").value;
+  let guess = Number(document.querySelector(".myguess").value);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "No number!";
-  } else if (guess < number) {
-    document.querySelector(".message").textContent = "Too low!";
+    messageElement.textContent = "No number 😟";
+  } else if (guess != number && !lost) {
     score--;
-    document.querySelector(".score").textContent--;
-  } else if (guess > number) {
-    document.querySelector(".message").textContent = "Too high!";
-    score--;
-    document.querySelector(".score").textContent--;
-  } else if (guess == number) {
-    document.querySelector(".message").textContent = "Correct!";
+    document.querySelector(".score").textContent = score;
+    lost = score == 0 ? 1 : 0;
+    messageElement.textContent =
+      guess > number && !lost ? "🛑 Too high" : "🛑 Too low";
+    messageElement.textContent = lost
+      ? "Lost game"
+      : messageElement.textContent;
+  } else if (guess == number && !lost) {
+    messageElement.textContent = "Correct! ✅";
+    document.querySelector(".number").textContent = number;
     if (score > highestScore) {
       highestScore = score;
       document.querySelector(".highest-score").textContent = highestScore;
-      document.querySelector(".right").style.backgroundColor = "#60b347";
+      bodyElement.style.backgroundColor = "#60b347";
     }
   }
 }
@@ -35,8 +39,13 @@ function verifyGuess() {
 function reset() {
   generateRandom();
   score = 20;
+  lost = 0;
+  highestScore = 0;
   document.querySelector(".score").textContent = score;
   document.querySelector(".myguess").value = "";
+  messageElement.textContent = "Guess away...";
+  bodyElement.style.backgroundColor = "rgb(48, 44, 44)";
+  document.querySelector(".number").textContent = "?";
 }
 
 document.querySelector(".btn-guess").addEventListener("click", verifyGuess);
